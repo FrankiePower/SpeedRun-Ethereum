973c443535e797290ef72b752eb2287f9669afbc
frankie-powers <franklin.power21@gmaul.com>
2024-11-01 22:44:41 +0100

    my token

--- challenge-2-token-vendor/packages/hardhat/contracts/YourToken.sol
+++ challenge-2-token-vendor/packages/hardhat/contracts/YourToken.sol
@@ -3,10 +3,17 @@ pragma solidity 0.8.4; //Do not change the solidity version as it negativly impa
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// learn more: https://docs.openzeppelin.com/contracts/4.x/erc20
 
 contract YourToken is ERC20 {
+
+address public owner = 0x0752f523512Ad24E82739D3434C0710A4cA5058f;
   constructor() ERC20("Gold", "GLD") {
-    //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+   
+    _mint(owner,1000 * 10 ** 18 );
+    
   }
+
+   function mint(uint _amount) external {
+        _mint(msg.sender,_amount * 1e18);
+    }
 }
